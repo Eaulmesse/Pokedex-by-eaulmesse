14ae9d67cc384a8f60ec2b82c1f39f8b932fbad1
eaulmesse <theo.guerin01120@gmail.com>
2022-06-27 16:51:39 +0200

    feat: processing to add list from gen

--- app.js
+++ app.js
@@ -45,3 +45,75 @@ function getPokemon() {
     })
 }
 
+
+let btnTest = document.getElementById('test');
+
+function createCard(ImgBackground,name,type,number) {
+    let globalSection = document.querySelector('.card-list');
+
+
+    let globalCard = document.createElement('div');
+    globalCard.classList.add("card-all-generate");
+    globalSection.appendChild(globalCard);
+    
+
+
+    let globalCardImg = document.createElement('div');
+    globalCardImg.classList.add("search-img");
+    globalCard.appendChild(globalCardImg);
+
+    let globalCardBackground = document.createElement('div');
+    globalCardBackground.classList.add("card-all-background");
+    globalCardBackground.style.backgroundImage = `url(${ImgBackground})`;
+    globalCardImg.appendChild(globalCardBackground);
+
+    let globalCardDesc = document.createElement('div');
+    globalCardDesc.classList.add("card-all-desc");
+    globalCard.appendChild(globalCardDesc);
+
+    let globalCardName = document.createElement('div');
+    globalCardName.classList.add("card-all-name");
+    globalCardName.textContent = name;
+    globalCardDesc.appendChild(globalCardName);
+
+    globalCardTypeNumber = document.createElement('div');
+    globalCardTypeNumber.classList.add("card-all-type-number");
+    globalCardDesc.appendChild(globalCardTypeNumber);
+    
+    globalCardType = document.createElement('div');
+    globalCardType.classList.add("card-all-type");
+    globalCardType.textContent = type
+    globalCardTypeNumber.appendChild(globalCardType);
+
+    globalCardNumber = document.createElement('div');
+    globalCardNumber.classList.add("card-all-number");
+    globalCardNumber.textContent = "n° " + number;
+    globalCardTypeNumber.appendChild(globalCardNumber);
+}
+
+const selectGen = document.getElementById('gen-select');
+let urlGen;
+
+selectGen.addEventListener("change", ()=> {
+    urlGen = `https://pokebuildapi.fr/api/v1/pokemon/generation/${selectGen.value}`
+    getList();
+})
+
+
+function getList() {
+    fetch(urlGen)
+    .then(response =>
+        response.json()
+        
+    ).then(data => {
+        console.log(data);
+
+
+        for (let pokemon of data) {
+            console.log(pokemon.apiTypes[0].name);
+            createCard(pokemon.image,pokemon.name,pokemon.apiTypes[0].name,pokemon.id)
+            
+        }
+    })
+}
+
